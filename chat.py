88e6422fc4559f48de459887eefc687f5def215b
Nikhil-Garakapati <nikhilgarakapati2829@gmail.com>
2018-09-20 01:49:24 +0530

    Test..Test..Test

--- chat.py
+++ chat.py
@@ -14,31 +14,6 @@ messages.insert(INSERT,'Agent:\n' + 'Welcome to the Chatbot-NLP\n\
     * Booking a Restaurent\n')
    
 
-input_user = StringVar()
-input_field = Entry(window, text=input_user)
-input_field.pack(side=BOTTOM, fill=X)
-
-
-bot = Session()
-def Enter_pressed(event):
-    input_get = input_field.get()
-    # print(input_get)
-    messages.insert(INSERT, 'You:\n%s\n\n\n' % input_get)
-    if(bot.flag==0):
-    	response = bot.reply(input_get)
-    	messages.insert(INSERT, 'Agent:\n'+str(response)+'\n\n')
-    	if response.startswith("Searching"):
-    		bot.flag = 1
-
-
-    if(bot.flag==1):
-    	response = bot.BookHotel(input_get)
-    	messages.insert(INSERT, 'Agent:\n'+str(response)+'\n\n')
-
-    # label = Label(window, text=input_get)
-    input_user.set('')
-    # label.pack()
-    return "break"
 
 frame = Frame(window)  # , width=300, height=300)
 input_field.bind("<Return>", Enter_pressed)
